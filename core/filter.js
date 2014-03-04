a263e815699c4fbd5f0fb4c204e3a17a9ed4b86b
Piotrek Reinmar Koszuliński <pkoszulinski@gmail.com>
2014-03-04 12:47:23 +0100

    Made filter understand wildcards.

--- core/filter.js
+++ core/filter.js
@@ -923,6 +923,23 @@
 		return !toBeRemoved.length;
 	}
 
+	// Returns a regexp object which can be used to test if a property
+	// matches one of wildcard validators.
+	function regexifyPropertiesWithWildcards( validators ) {
+		var patterns = [],
+			i;
+
+		for ( i in validators ) {
+			if ( i.indexOf( '*' ) > -1 )
+				patterns.push( i.replace( /\*/g, '.*' ) );
+		}
+
+		if ( patterns.length )
+			return new RegExp( '^(?:' + patterns.join( '|' ) + ')$' );
+		else
+			return null;
+	}
+
 	// Returns function that accepts {@link CKEDITOR.htmlParser.element}
 	// and filters it basing on allowed content rules registered by
 	// {@link #allow} method.
@@ -1379,8 +1396,13 @@
 		if ( validator === true )
 			return true;
 
+		// Note: We don't need to remove properties with wildcards from the validator object.
+		// E.g. data-* is actually an edge case of /^data-.*$/, so when it's accepted
+		// by `value in validator` it's ok.
+		var regexp = regexifyPropertiesWithWildcards( validator );
+
 		return function( value ) {
-			return value in validator;
+			return value in validator || ( regexp && value.match( regexp ) );
 		};
 	}
 

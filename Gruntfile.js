edf58d6c6b5f306fb354717bdceb0e17bfd193f7
nilesh.tochia <nilesh.tochia@axonn.co.uk>
2014-09-19 17:16:19 +0100

    Adds task to concatenate image3 dialog

---
+++ Gruntfile.js
@@ -0,0 +1,27 @@
+﻿'use strict';
+module.exports = function(grunt) {
+    grunt.initConfig({
+        concat: {
+            dist: {
+                src: [
+                    'dev/builder/release/ckeditor/ckeditor.js',
+                    'dev/builder/release/ckeditor/plugins/image3/{,*/}*.js',
+                ],
+                dest: 'dev/builder/release/ckeditor/ckeditor.min.js'
+            }
+        }, 
+        copy: {        
+            dist: {
+                expand: true,     
+                cwd: 'dev/builder/release/ckeditor',
+                // src: ['**', '!plugins/**', '!lang'],
+                src: 'ckeditor.min.js',
+                dest: 'release/ckeditor'
+            }
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-copy');
+
+}
